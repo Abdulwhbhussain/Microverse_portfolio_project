2aec93988ff81aa58d2a00a360bdfbae76ada288
WHB901 <WHB901@gmail.com>
2023-04-12 00:03:58 +0500

    Add the PopUp Window

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
 const hamBurgerElement = document.querySelector('.toolbar-menu-container');
 const overlayMenu = document.querySelector('.overlay-menu');
 
-console.log(hamBurgerElement.nodeName);
-console.log(hamBurgerElement.nodeType);
-console.log(hamBurgerElement.classList);
-
-console.log(overlayMenu.nodeName);
-console.log(overlayMenu.nodeType);
-console.log(overlayMenu.classList);
-
 hamBurgerElement.addEventListener('click', () => {
   overlayMenu.classList.toggle('hidden');
 });
-
-console.log(overlayMenu.classList);
 
 overlayMenu.addEventListener('click', () => {
   overlayMenu.classList.toggle('hidden');
 });
 
-const overlayWindow = document.querySelector('#overlay');
+// Pop Up Window
 
 document.querySelectorAll('.show-modal').forEach((element) => {
   element.addEventListener('click', () => {
