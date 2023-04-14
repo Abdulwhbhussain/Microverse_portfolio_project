6778b1cdd2c719cd47462215787b9bd39975b166
WHB901 <WHB901@gmail.com>
2023-04-14 18:23:31 +0500

    Dynamically Generated Work Section and Pop Up Windows for Project

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ for (let i = 0; i < projects.length; i += 1) {
 
 // Pop up window work section
 
-for (let i=0; i < projects.length; i += 1) {
+for (let i = 0; i < projects.length; i += 1) {
   const overlay = document.createElement('div');
   overlay.className = `overlay-${i}`;
   overlay.innerHTML = `
@@ -178,13 +178,12 @@ for (let i=0; i < projects.length; i += 1) {
   document.querySelector('.template-1').appendChild(overlay);
 }
 
-for (let i = 0; i < projects.length; i += 1) {
-  
-  const overlayy = document.querySelector(`.overlay-${i}`)
+for (let i = 0; i < projects.length; i += 1) {  
+  const overlayy = document.querySelector(`.overlay-${i}`);
 
-    document.querySelector(`#click-id-${i}`).addEventListener('click', () => {
-        overlayy.style.display = 'block';
-      }); 
+  document.querySelector(`#click-id-${i}`).addEventListener('click', () => {
+    overlayy.style.display = 'block';
+  }); 
 
 }
 
